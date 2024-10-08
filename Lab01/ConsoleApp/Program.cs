ffa1654c9d39a373caabe993caeb35a3de14e72a
klaudiusz.jedrzejczy <klaudiusz.jedrzejczy@CH-11>
2024-10-08 17:43:00 +0200

    Dodaj pliki projektów.

---
+++ Lab01/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿//Console.WriteLine("Hello, World!");
+
+string message = "";
+message += "Hello";
+message += ",";
+message += " ";
+message += "World!";
+Console.WriteLine(message);
+
+Console.WriteLine("Przejście do\nnowej linii.");
+Console.WriteLine("Tabulacja\tpozioma.");
+Console.WriteLine("Tabulacja\npionowa.");
+Console.WriteLine("Alarm!\a");
+Console.WriteLine("Pojedynczy apostrof: \'");
+Console.WriteLine("Pojedyczny cudzysłów: \"");
+Console.WriteLine("Znak pusty: \0");
+
+Console.Beep();
