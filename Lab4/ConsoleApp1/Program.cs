4f53b4cebc5661f0fea99d43f08124a2f5f037c4
Klaudiusz Jędrzejczyk <klaudiusz.jedrzejczy@microsoft.wsei.edu.pl>
2024-11-19 17:03:44 +0100

    lab04 + spr

---
+++ Lab4/ConsoleApp1/Program.cs
@@ -0,0 +1,158 @@
+﻿using ConsoleApp1;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //  Zadanie 1
+            Console.WriteLine(QuadraticEquation(1, 4, 8, 4));
+
+            //  Zadanie 2
+            double x = 10;
+            double y = 20;
+            Swap(ref x, ref y);
+            Console.WriteLine($"x = {x}, y = {y}");
+
+            //  Zadanie 3
+            Console.WriteLine(Multiply(1.0, 5.0));
+            Console.WriteLine(Multiply(1.0, 5.0, 3.0));
+            Console.WriteLine(Multiply(1.0, 2.0, 2.0, 2.0, 2.0));
+
+            //  Zadanie 4
+            PrintDayOfTheWeek(DayOfTheWeek.Saturday);
+
+            //  Zadanie 5
+            DateTime now = DateTime.Now;
+            Console.WriteLine(ShowFullDate(now));
+
+            //  Zadanie 6
+            Console.WriteLine(Devide(5.0, 0.0));
+
+            Console.WriteLine(SetYourNewPassword("123456789Ab!"));
+        }
+
+        //  Zadanie 1
+        public static double QuadraticEquation(double x = 0.0, double a = 1.0, double b = 1.0, double c = 1.0)
+        {
+            return a * x * x + b * x + c;
+        }
+
+        //  Zadanie 2
+        public static void Swap(ref double a, ref double b)
+        {
+            double temp = a;
+            a = b;
+            b = temp;
+        }
+
+        //  Zadanie 3
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Multiply(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+        public static double Multiply(params double[] list)
+        {
+            double result = 1;
+            foreach (double x in list)
+            {
+                result += x;
+            }
+            return result;
+        }
+
+        //  Zadanie 4
+        enum DayOfTheWeek : ushort
+        {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
+        }
+        static void PrintDayOfTheWeek(DayOfTheWeek day)
+        {
+            Console.WriteLine($"{day} = {(int)day}");
+            if ((int)day < 6)
+            {
+                Console.WriteLine("Praca :(");
+            }
+            else
+            {
+                Console.WriteLine("Wolne :)");
+            }
+        }
+
+        //  Zadanie 5
+        public static string ShowFullDate(DateTime date)
+        {
+            return $"{date.Day}.{date.Month}.{date.Year} {date.Hour}:{date.Minute}:{date.Second} {date.DayOfWeek}";
+        }
+
+        //  Zadanie 6
+        public static double Devide(double a, double b)
+        {
+            try
+            {
+                return a / b;
+            }
+            catch (DivideByZeroException e)
+            {
+                return 0.0;
+            }
+        }
+
+        //  Zadanie 7
+        public static string SetYourNewPassword(string password)
+        {
+            var upperCapsRegex = new Regex(@"[A-Z]");
+            var lowerCapsRegex = new Regex(@"[a-z]");
+            var specialCharsRegex = new Regex(@"\|!#%&/()=»«@£§€{}-;'<>_,");
+            var numberRegex = new Regex(@"[0-9]");
+
+            if (password == "")
+            {
+                throw new ArgumentException("Brak hasła");
+            }
+
+            if (password.Length < 10)
+            {
+                throw new FormatException("Minimalnie 10 znaków");
+            }
+
+            if (!upperCapsRegex.IsMatch(password))
+            {
+                throw new FormatException("Musi zawierać duża literę");
+            }
+
+            if (!lowerCapsRegex.IsMatch(password))
+            {
+                throw new FormatException("Musi zawierać mała literę");
+            }
+
+            if (!specialCharsRegex.IsMatch(password))
+            {
+                throw new FormatException("Musi zawierać znak specjalny");
+            }
+
+            if (!numberRegex.IsMatch(password))
+            {
+                throw new FormatException("Musi zawierać cyfrę");
+            }
+
+
+            return password;
+        }
+    }
+}
---
+++ Sprawdzian/ConsoleApp1/Program.cs
@@ -0,0 +1,147 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rozpoczynam sprawdzian)");
+            Zadanie_5_6();
+            Console.WriteLine("Kończę sprawdzian.");
+        }
+
+        static void Zadanie_1()
+        {
+            Console.WriteLine("Rozpoczynam zadanie 1");
+            int a = 11;
+            int b = 123;
+            
+            for(int i = a; i <= b; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Kończę zadanie 1");
+        }
+
+        static void PrintTriangle(int h) 
+        {
+            string symbol = "$";
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = h; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(symbol);
+                symbol += "$$";
+            }
+        }
+
+        static void Zadanie_2()
+        {
+            Console.WriteLine("Rozpoczynam zadanie 2");
+            // zadanie numer 2
+            // zaimplementuj poniższą procedurę
+            PrintTriangle(10);
+            Console.WriteLine("Kończę zadanie 2");
+        }
+        static void Zadanie_3()
+        {
+            Console.WriteLine("Rozpoczynam zadanie 3");
+            DateTime today = DateTime.Now;
+
+            // tutaj rozwiązanie zadania 3
+            string finalText = "";
+
+            if(today.Hour < 12)
+            {
+                finalText = "Dzień dobry, ";
+            }
+            else
+            {
+                finalText = "Dobry wieczór, ";
+            }
+            finalText += $"dzisiaj {today.DayOfWeek}, czyli {(int)today.DayOfWeek} dzień tygodnia";
+
+            Console.WriteLine(finalText);
+
+            Console.WriteLine("Kończę zadanie 3");
+        }
+
+        static double FindMaxAbsoluteValue(double[] args)
+        {
+            double max = 0;
+            foreach(double x in args)
+            {
+                if(Math.Abs(x) > max) 
+                    { max = x; }
+            }
+            return max;
+        }
+
+        static void Zadanie_4()
+        {
+            Console.WriteLine("Rozpoczynam zadanie 4");
+            double[] numbers = { 1.5, -2.22, 4.56, -7, 8.01, 0.0 };
+
+            // zadanie numer 4
+            // zaimplementuj poniższą funkcję
+            Console.WriteLine("The MIN value = " + FindMaxAbsoluteValue(numbers));
+            Console.WriteLine("Kończę zadanie 4");
+        }
+
+
+        static void Zadanie_5_6()
+        {
+            // tutaj rozwiązanie zadań 5 oraz 6
+            // można modyfikować, zmieniać kolejność itp.
+
+            Console.WriteLine("Rozpoczynam pętlę.");
+
+            int number = 0;
+            do
+            {
+                Console.WriteLine("Podaj numer zadania od 1 do 4, które mam wyświetlić. (0 - koniec)");
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+
+                    if (number == 1)
+                    {
+                        Zadanie_1();
+                    }
+                    else if (number == 2)
+                    {
+                        Zadanie_2();
+                    }
+                    else if (number == 3)
+                    {
+                        Zadanie_3();
+                    }
+                    else if (number == 4)
+                    {
+                        Zadanie_4();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie ma takiego zadania, wybierz ponownie.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Wartość nie jest cyfrą!");
+                }
+            }
+            while (number != 0);
+            
+
+            Console.WriteLine("Kończę pętlę.");
+        }
+
+    }
+}
